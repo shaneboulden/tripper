8521df5c1f3905bb5d2a61ecfb4e3e6e7c6c9d43
Shane Boulden <shane.boulden@gmail.com>
2015-07-17 19:20:45 +1000

    initial files

---
+++ main.rb
@@ -0,0 +1,93 @@
+require 'sinatra'
+require 'twitter'
+require 'chartkick'
+require 'sinatra/flash'
+#require 'googlecharts'
+#require 'gchart'
+
+enable :sessions
+
+get '/' do
+  @title = 'Search'
+  erb :home
+end
+
+post '/profile' do
+      @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = '***REMOVED***'
+      config.consumer_secret = '***REMOVED***'
+      config.access_token = '***REMOVED***'
+      config.access_token_secret = '***REMOVED***'
+    end
+      
+      @title='Search'
+
+      begin
+        #create the user and show the profile pag
+        @user = @client.user(params[:content]) 
+     
+        # create the objects for the chartkick charts
+     
+        @tweetsource ||= Hash.new(0)
+        @retweet_total = 0
+        @num_tweets = 0
+        @tweetday ||= {"Sunday" => 0, "Monday" => 0, "Tuesday" => 0,
+                    "Wednesday" => 0, "Thursday" => 0, "Friday" => 0,
+                    "Saturday" => 0}
+        @followtz ||= Hash.new(0)
+        @sourcedays = Hash.new(0)
+        @tweetline = Hash.new(0)
+
+        #note that the user_timeline method returns the 20 most recent Tweets 
+        #posted by the specified user - rate limiting?
+        timeline = @client.user_timeline(@user.screen_name, :count => 200)
+        timeline.each do |t|
+          @num_tweets += 1
+          @retweet_total += t.retweet_count
+          date = Date.parse(t.created_at.to_s[0..9])
+          @tweetday[date.strftime('%A')] += 1
+      
+          # use a regular expression to get the text enclosed by 
+          # >< in the twitter source, then remove the first and last chars
+          src = />(.*)</.match(t.source)
+          @tweetsource[src[0][1..-2]] += 1
+
+          @tweetline[date] += 1
+
+          #push the date and src onto a hash
+          #@sourcedays[src[0][1..-2]] += [date,1]
+          #puts "date: #{date}, src: #{src[0][1..-2]}"
+          # note data needs to be of the form [
+          #   {name: "series A", data: series_a},
+          #   {name: "series B", data: series_b}
+          # ]
+        end
+
+      erb :profile
+    
+      rescue Twitter::Error::NotFound => e
+          flash[:error] = e.message
+          redirect '/'
+      rescue Twitter::Error::Forbidden => e
+          flash[:error] = e.message
+          redirect '/'
+      end
+end
+
+post "/map" do
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = '***REMOVED***'
+      config.consumer_secret = '***REMOVED***'
+      config.access_token = '***REMOVED***'
+      config.access_token_secret = '***REMOVED***'
+    end
+ 
+   @title = "geomap"
+   @geo_lat = params[:geo_lat]
+   @geo_long = params[:geo_long]
+
+   #Array @geotweets = @client.search(" -rt",:geocode => "{#@geo_lat},{#@geo_long}, 5km").take(3).collect
+
+   Array @geotweets = @client.search("Phil Walsh -rt", :result_type => "recent").take(3).collect
+   erb :map
+end    
